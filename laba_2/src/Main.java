2a04f9b6adb16a62095c688ad6ca464e67590f2f
Alex Varvashevich <146416031+leshhik@users.noreply.github.com>
2023-12-18 22:05:13 +0300

    the second laboratory

---
+++ laba_2/src/Main.java
@@ -0,0 +1,16 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        formula formula = new formula();
+        formula.setVisible(true);
+    }
+}
